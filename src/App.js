24d24abe26eb55646da0959fd12b9c0544cce3fb
Hélèna Thomassin <thomassin.helena@gmail.com>
2019-10-08 16:45:42 +0200

    ajout contact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact 
+        name="Anita Sutton"
+        avatar="https://randomuser.me/api/portraits/women/21.jpg"
+        isOnline
+      />
+      <Contact 
+        name="Jim George"
+        avatar="https://randomuser.me/api/portraits/men/46.jpg"
+        isOnline
+      />
+      <Contact 
+        name="Charlotte Ryan"
+        avatar="https://randomuser.me/api/portraits/women/92.jpg"
+        isOnline
+      />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Contact.css'
+
+function Contact(props) {
+    return(
+        <div className="Contact">
+            <img src={props.avatar}/>
+            <div>
+            <h4>{props.name}</h4>
+            {props.isOnline ?
+                <div className="status">
+                    <div className="status-online" /><p className="status-text">Online</p>
+                </div> : 
+                <div className="status">
+                    <div className="status-offline" /><p className="status-text">Offline</p>
+                </div>
+            }
+            </div>
+        </div>
+);
+}
+
+export default Contact;
