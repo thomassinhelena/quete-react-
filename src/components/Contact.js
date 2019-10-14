d3ee7137e258fb4c02f299a464a8e842464e3019
Hélèna Thomassin <thomassin.helena@gmail.com>
2019-10-14 13:49:20 +0200

    expedition state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import './Contact.css'
 
-function Contact(props) {
+class Contact extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isOnLine: props.isOnline,
+    };
+  }
+  changeOnLine = () => {
+    const newOnLine = !(this.state.isOnLine)
+    this.setState({isOnLine:newOnLine});
+  }
+  render() {
     return(
-        <div className="Contact">
-            <img alt="" src={props.avatar}/>
+      <div className="Contact">
+            <img alt="" src={this.props.avatar}/>
             <div>
-            <h4>{props.name}</h4>
-            {props.isOnline ?
+            <h4>{this.props.name}</h4>
+            {this.state.isOnLine ?
                 <div className="status">
-                    <div className="status-online" /><p className="status-text">Online</p>
+                    <div className="status-online" /><p onClick = {this.changeOnLine} className="status-text">Online</p>
                 </div> : 
                 <div className="status">
-                    <div className="status-offline" /><p className="status-text">Offline</p>
+                    <div className="status-offline" /><p onClick = {this.changeOnLine}className="status-text">Offline</p>
                 </div>
             }
             </div>
         </div>
-);
-}
+    );
+  }
+}    
 
 export default Contact;
